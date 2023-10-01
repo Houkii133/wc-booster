107fd189b23a3fe7f34fb1087ed1bb917b4e6dcf
RajaramBhurtel <sabinbhurtel@gmail.com>
2023-10-01 22:21:50 +0545

    initial commit

---
+++ patterns/about.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Title: About
+ * Slug: wc-booster/about
+ * Categories: wc-booster
+ *
+ * @package WC Booster
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:columns {"align":"full"} -->
+<div class="wp-block-columns alignfull"><!-- wp:column {"width":"50%"} -->
+<div class="wp-block-column" style="flex-basis:50%"><!-- wp:cover {"customOverlayColor":"#f6f6f6","minHeight":600,"isDark":false} -->
+<div class="wp-block-cover is-light" style="min-height:600px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim" style="background-color:#f6f6f6"></span><div class="wp-block-cover__inner-container"><!-- wp:image {"align":"center","id":61,"height":600,"aspectRatio":"9/16","scale":"contain","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/cafe-road-street-town-building-restaurant-99136-pxhere.com.jpg" alt="" class="wp-image-61" style="aspect-ratio:9/16;object-fit:contain;height:600px" height="600"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"1em","right":"1em","bottom":"1em","left":"1em"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:1em;padding-right:1em;padding-bottom:1em;padding-left:1em"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"320px"} -->
+<div class="wp-block-column" style="flex-basis:320px"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.6"}},"fontSize":"small"} -->
+<p class="has-small-font-size" style="line-height:1.6"><strong><?php echo esc_html__( 'Museum Restro', 'wc-booster' ); ?></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'Star Restro is a museum Restaurant dedicated to famous Turkish impressionist Rami Uluer. Since 1989 our guests enjoy of many examples of Rami Uluer’s works gracing the walls of Rami Restaurant.', 'wc-booster' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( 'We only use authentic ingredients in our kitchen.', 'wc-booster' ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"normal"} -->
+<p class="has-text-align-center has-normal-font-size"><?php echo esc_html__( 'Collaborating with international chefs and restaurateurs, we have established a new menu and philosophy around supporting local producers in a safe and sustainable way to encourage the development of standardized and trusted produce in Nepal.', 'wc-booster' ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":45,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/working-cafe-coffee-shop-coffee-people-restaurant-1224792-pxhere.com.jpg" alt="" class="wp-image-45"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":46,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/table-cutlery-silverware-wine-restaurant-dish-892507-pxhere.com.jpg" alt="" class="wp-image-46"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":48,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/work-restaurant-steel-busy-meal-food-1327589-pxhere.com.jpg" alt="" class="wp-image-48"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":47,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/cafe-road-street-town-building-restaurant-99136-pxhere.com.jpg" alt="" class="wp-image-47"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":50,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/wood-vintage-retro-window-glass-old-1169016-pxhere.com.jpg" alt="" class="wp-image-50"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":49,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/table-wood-vintage-retro-chair-restaurant-1169007-pxhere.com.jpg" alt="" class="wp-image-49"/></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Star Restro | Phone:&nbsp;<a href="tel:+902125176593">(0212) 517 65 93</a>&nbsp;| Address:&nbsp;<a rel="noreferrer noopener" href="#" target="_blank">Sultan Ahmet, Utangaç Sk. No:4, 34122 Fatih/İstanbul</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
