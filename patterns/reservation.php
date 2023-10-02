c69a89aacdac1af1fe77d5b9b3b38c540fee24dd
Raja Ram Bhurtel <sabinbhurtel@gmail.com>
2023-10-02 15:45:27 +0545

    menu and reservation pattern

---
+++ patterns/reservation.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Reservation
+ * Slug: wc-booster/reservation
+ * Categories: wc-booster
+ *
+ * @package WC Booster
+ * @since 1.0.0
+ */
+
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"var:preset|spacing|60","bottom":"0","left":"var:preset|spacing|60"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--60);padding-bottom:0;padding-left:var(--wp--preset--spacing--60)"><!-- wp:cover {"url":"<?php echo esc_url( get_template_directory_uri() );?>/assets/images/table-wood-house-restaurant-home-residence-670077-pxhere.com_.jpg","id":50,"dimRatio":10,"overlayColor":"background","minHeight":450,"minHeightUnit":"px","contentPosition":"center center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;min-height:450px"><span aria-hidden="true" class="wp-block-cover__background has-background-background-color has-background-dim-10 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-50" alt="" src="<?php echo esc_url( get_template_directory_uri() );?>/assets/images/table-wood-house-restaurant-home-residence-670077-pxhere.com.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|30","left":"var:preset|spacing|30","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--30);padding-bottom:0;padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"style":{"spacing":{"margin":{"top":"120px"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:120px"><!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0px","bottom":"0px","left":"0"}},"typography":{"fontStyle":"normal","fontWeight":"200","lineHeight":"1.4"}},"textColor":"background","fontSize":"extra-large","fontFamily":"oregano"} -->
+<p class="has-text-align-center has-background-color has-text-color has-oregano-font-family has-extra-large-font-size" style="margin-top:0px;margin-bottom:0px;margin-left:0;font-style:normal;font-weight:200;line-height:1.4"><em><?php  esc_html_e( 'Join Us For', 'wc-booster' ); ?></em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"10px","bottom":"10px"}}},"textColor":"background","fontSize":"extra-small","fontFamily":"montserrat"} -->
+<p class="has-text-align-center has-background-color has-text-color has-montserrat-font-family has-extra-small-font-size" style="margin-top:10px;margin-bottom:10px"><strong><?php  esc_html_e( 'Breakfast | Lunch | Dinner', 'wc-booster' ); ?></strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"10px","bottom":"10px"},"blockGap":"0"}}} -->
+<div class="wp-block-buttons" style="margin-top:10px;margin-bottom:10px"><!-- wp:button {"backgroundColor":"primary","style":{"border":{"radius":"0px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-primary-background-color has-background wp-element-button" style="border-radius:0px"><?php  esc_html_e( 'Reservation', 'wc-booster' ); ?></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer {"height":"60px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:60px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
